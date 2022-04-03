d2851909936e0d289ac4a591527003151751f011
Jintao Koong <jintaokoong99@gmail.com>
2022-04-03 22:20:16 +0800

    ✨ add unit testing

---
+++ src/calculations/math.ts
@@ -0,0 +1,14 @@
+const add =
+  (x: number) =>
+  (y: number): number =>
+    x + y;
+
+const subtract =
+  (x: number) =>
+  (y: number): number =>
+    x - y;
+
+export default {
+  add,
+  subtract,
+};
---
+++ tests/calculations/math.test.ts
@@ -0,0 +1,12 @@
+import math from '../../src/calculations/math';
+
+describe('calculate', function () {
+  it('add', function () {
+    const result = math.add(1)(2);
+    expect(result).toBe(3);
+  });
+  it('subtract', function () {
+    const result = math.subtract(1)(2);
+    expect(result).toBe(-1);
+  });
+});
